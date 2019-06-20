eb485289086b52f9989f44c006aea872bae2b80a
João Monteiro <joao.monteirodemedeiros@gmail.com>
2019-06-20 16:46:20 -0300

    first commit

--- arvbm.c
+++ arvbm.c
@@ -30,94 +30,19 @@ void libera(TABM *a){
 }
 
 TABM *busca(TABM *a, int mat){
-  if (!a) return NULL;
-  int i = 0;
-  while ((i < a->nchaves) && (mat > a->chave[i])) i++;
-  if ((i < a->nchaves) && (a->folha) && (mat == a->chave[i])) return a;
-  if (a-> folha) return NULL;
-  if (a->chave[i] == mat) i++;
-  return busca(a->filho[i], mat);
 }
 
 void imprime(TABM *a, int andar){
-  if(a){
-    int i,j;
-    for(i=0; i<=a->nchaves-1; i++){
-      imprime(a->filho[i],andar+1);
-      for(j=0; j<=andar; j++) printf("   ");
-      printf("%d\n", a->chave[i]);
-    }
-    imprime(a->filho[i],andar+1);
-  }
 }
 
 TABM *divisao(TABM *x, int i, TABM* y, int t){
-  TABM *z = cria(t);
-  z->folha = y->folha;
-  int j;
-  if(!y->folha){
-    z->nchaves = t-1;
-    for(j=0;j<t-1;j++) z->chave[j] = y->chave[j+t];
-    for(j=0;j<t;j++){
-      z->filho[j] = y->filho[j+t];
-      y->filho[j+t] = NULL;
-    }
-  }
-  else {
-    z->nchaves = t; //z possuir� uma chave a mais que y se for folha
-    for(j=0;j < t;j++) z->chave[j] = y->chave[j+t-1];//Caso em que y � folha, temos q passar a info para o n� da direita
-    y->prox = z;
-  }
-  y->nchaves = t-1;
-  for(j=x->nchaves; j>=i; j--) x->filho[j+1]=x->filho[j];
-  x->filho[i] = z;
-  for(j=x->nchaves; j>=i; j--) x->chave[j] = x->chave[j-1];
-  x->chave[i-1] = y->chave[t-1];
-  x->nchaves++;
-  return x;
 }
 
 
 TABM *insere_nao_completo(TABM *x, int mat, int t){
-  int i = x->nchaves-1;
-  if(x->folha){
-    while((i>=0) && (mat < x->chave[i])){
-      x->chave[i+1] = x->chave[i];
-      i--;
-    }
-    x->chave[i+1] = mat;
-    x->nchaves++;
-    return x;
-  }
-  while((i>=0) && (mat < x->chave[i])) i--;
-  i++;
-  if(x->filho[i]->nchaves == ((2*t)-1)){
-    x = divisao(x, (i+1), x->filho[i], t);
-    if(mat > x->chave[i]) i++;
-  }
-  x->filho[i] = insere_nao_completo(x->filho[i], mat, t);
-  return x;
 }
 
 TABM *insere(TABM *T, int mat, int t){
-  if(busca(T, mat)) return T;
-  if(!T){
-    T=cria(t);
-    T->chave[0] = mat;
-    T->nchaves=1;
-    return T;
-  }
-  if(T->nchaves == (2*t)-1){
-    TABM *S = cria(t);
-    S->nchaves=0;
-    S->folha = 0;
-    S->filho[0] = T;
-    S = divisao(S,1,T,t);
-    S = insere_nao_completo(S, mat, t);
-    return S;
-  }
-  T = insere_nao_completo(T, mat, t);
-  return T;
 }
 
 
